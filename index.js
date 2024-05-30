cc0d87b01306497adb43c3630a1795b266f4c53d
aayushwebdev <aayushmesh05@gmail.com>
2024-05-30 18:32:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.getElementById("progress");
+const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-step");
+
+let formStepsNum = 0;
+
+nextBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressbar();
+  });
+});
+
+prevBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum--;
+    updateFormSteps();
+    updateProgressbar();
+  });
+});
+
+function updateFormSteps() {
+  formSteps.forEach((formStep) => {
+    formStep.classList.contains("form-step-active") &&
+      formStep.classList.remove("form-step-active");
+  });
+
+  formSteps[formStepsNum].classList.add("form-step-active");
+}
+
+function check() {
+    let check = document.getElementById("yes").checked;
+    if(check = true) {
+        document.getElementsById("upload-img").style.display = "block";
+    }else {
+        document.getElementsById("upload-img").style.display = "none";
+    }
+}
+
+check();
